3b8e089d9597cd1bd732222d6406bb43247e9af4
henry <henry.s.blue@gmail.com>
2022-10-25 10:14:01 +0300

    initial commit

---
+++ part1-TravelPlanner/src/citymap.py
@@ -0,0 +1,91 @@
+from collections import deque
+import json
+import os
+
+
+def load_data(source_file):
+    """Reading JSON from the file and deserializing it
+    :param source_file: JSON map of tram stops (str)
+    :return: obj (array)
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir_path, '..', source_file)) as file:
+        return json.load(file)
+
+
+class State:
+
+    def __init__(self, stop, previous=None):
+        """ State lets you trace back the route from the last stop
+        :attr stop: Code of the stop (str)
+        :attr previous: Previous state of the route (State)
+        """
+        self.stop = stop
+        self.previous = previous
+
+
+    def __str__(self):
+        """ Returns route as a string from the last stop to the beginning. 
+            Format: 1030423 -> 1010420 -> 1010427 
+        """
+        result = self.stop
+        state = self.previous
+        while state is not None:
+            result += " -> " + state.stop
+            state = state.previous
+
+        return result
+
+    def get_stop(self):
+        return self.stop
+
+    def get_previous(self):
+        return self.previous
+
+
+class CityMap:
+    """Storage of the tram network stops
+    :attr data: (obj)
+    :attr stops: dictionary {stop_code: stop}
+    """
+    def __init__(self, source_file):
+        self.data = load_data(source_file)
+        self.stops = {}
+        for stop in self.data:
+            self.stops[stop["code"]] = stop
+
+    def get_neighbors(self, stop_code):
+        """Returns dictionary containing all neighbor stops """
+        return self.stops.get(stop_code)["neighbors"]
+
+    def get_neighbors_codes(self, stop_code):
+        """Returns codes of all neighbor stops """
+        return list(self.stops.get(stop_code)["neighbors"].keys())
+
+    def search(self, start, goal):
+        """Implement breadth-first search. Return the answer as a linked list of States
+        where the first node contains the goal stop code and each node is linked to the previous node in the path.
+        The last node in the list is the starting stop and its previous node is None.
+
+        :param start: Code of the initial stop (str)
+        :param goal: Code of the last stop (str)
+        :returns (obj)
+        """
+        
+        queue = deque()
+        queue.append(State(start))
+ 
+        visited=[start]
+ 
+        while len(queue) != 0:
+            cur_road=queue.popleft()
+            neighbors = self.get_neighbors(cur_road.get_stop())
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    state = State(neighbor,cur_road)
+                    if neighbor == goal:
+                        return state
+                    queue.append(state)
+                    visited.append(neighbor)
+
+        return None
---
+++ part2-AlphaBeta/src/alphabeta.py
@@ -0,0 +1,109 @@
+TEMPLATE_FIELD = '|e|e|e|\n|e|e|e|\n|e|e|e|\n'
+HUGE_NUMBER = 1000000
+
+
+class AlphaBetaNode(object):
+    def __init__(self):
+        pass
+
+    def generate_children(self):
+        pass
+
+    def is_max_node(self):
+        pass
+
+    def is_end_state(self):
+        pass
+
+    def value(self):
+        pass 
+
+
+class TicTacToe(AlphaBetaNode):
+    """Class that contains current state of the game and implements AlphaBetaNode methods
+    :attr state: Current state of the board (str)
+    :attr state: Indicates whose turn it is (Boolean)
+    """
+
+    def __init__(self, state, crosses_turn):
+        super().__init__()
+        self.state = state
+        self.crosses_turn = crosses_turn
+
+    def is_end_state(self):
+        return ('?' not in self.state) or self.won('x') or self.won('o')
+
+    def won(self, c):
+        triples = [self.state[0:3], self.state[3:6], self.state[6:9], self.state[::3], self.state[1::3],
+                   self.state[2::3], self.state[0] + self.state[4] + self.state[8],
+                   self.state[2] + self.state[4] + self.state[6]]
+        combo = 3 * c
+        return combo in triples
+
+    def __str__(self):
+        field = TEMPLATE_FIELD
+        for c in self.state:
+            field = field.replace('e', c, 1)
+        return field
+
+    def is_max_node(self):
+        return self.crosses_turn
+
+    def generate_children(self):
+        """
+        Generates list of all possible states after this turn
+        :return: list of TicTacToe objects
+        """
+        children=[]
+        for x in range(9):
+            if self.state[x] == '?':
+                child = self.state[0:x] + ('x' if self.crosses_turn else 'o') + self.state[x+1:9]
+                children.append(TicTacToe(child, (False if self.crosses_turn else True)))
+
+        return children
+
+    def value(self):
+        """
+        Current score of the game (0, 1, -1)
+        :return: int
+        """
+
+        if ('?' not in self.state): return 0
+        elif self.won('x'): return 1
+        elif self.won('o'): return -1
+
+
+
+def alpha_beta_value(node):
+    """Implements the MinMax algorithm with alpha-beta pruning, this function sees whos turn it is in the initial state (Xs or Os) and calls either a min_value search or a max value search using alpha beta peramiters.
+
+    :param node: State of the game (TicTacToe)
+    :return: int
+    """
+    if node.crosses_turn: 
+        return max_value(node, (-HUGE_NUMBER) , HUGE_NUMBER )
+    else: 
+        return min_value(node, (-HUGE_NUMBER) , HUGE_NUMBER )
+
+
+def max_value(node, alpha, beta):
+    """
+    
+    """
+    if node.is_end_state(): return node.value()
+    v = -HUGE_NUMBER
+    for child in node.generate_children():
+       v = max(v, min_value(child, alpha, beta))
+       alpha = max(alpha, v)
+       if alpha >= beta: return v
+    return v
+
+
+def min_value(node, alpha, beta):
+    if node.is_end_state(): return node.value()
+    v = HUGE_NUMBER
+    for child in node.generate_children():
+       v = min(v, max_value(child, alpha, beta))
+       beta = min(beta, v)
+       if alpha >= beta: return v
+    return v
